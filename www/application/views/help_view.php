e74e585bb7a4ec9235d3caefddb075133bd9bb5b
Alexander Kaznacheev <aakzn@mail.ru>
2015-04-23 09:43:12 +0300

    Release #1 Рабочая версия

---
+++ www/application/views/help_view.php
@@ -0,0 +1,58 @@
+
+<!--<div class="section no-pad-bot" id="index-banner">
+<div class="container">
+<br><br>
+<h1 class="header center orange-text">Starter Template</h1>
+<div class="row center">
+<h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+</div>
+<div class="row center">
+<a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+</div>
+<br><br>
+
+</div>
+</div>-->
+
+<div class="container">
+<div class="section">
+
+<!— Icon Section —>
+<div class="row">
+<!--<div class="col s12 m4">
+<div class="icon-block">
+<h2 class="center light-blue-text"><i class="mdi-image-flash-on"></i></h2>
+<h5 class="center">Speeds up development</h5>
+
+<p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+</div>
+</div>-->
+
+<div class="col s16 m12 center" >
+<div class="text">
+<span class="purple-text"><h4>Чтобы пользоваться нашим сервисом, вы должны иметь фотографии на электронном носителе. На <a class="blue-text" href="/upload">странице заказа</a> нужно указать свои данные, загрузить фотографии. После этого вы получите уведомление, и если все успешно, то получите номер заказа, который необходимо сохранить. Номер заказа используется для получения фотографий.</h4>
+</span>
+
+</div>
+
+</div>
+
+<div class="col s16 m12" >
+<div class="text">
+<span class="purple-text"><h4>Остались вопросы? Звоните! 8-499-24-23-12</h4>
+</span>
+
+</div>
+
+</div>
+
+
+</div>
+
+</div>
+<br><br>
+
+<!--<div class="section">
+
+</div>-->
+</div>
